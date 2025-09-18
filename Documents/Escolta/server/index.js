e720a1014e6dcff9d6ecb3c8d4988b72211915a5
Gustavo Izzo Admin <_gustavo.izzo@internal.safewayconsultoria.com>
2025-09-18 19:18:48 -0300

    Change backend port to 3001 to avoid conflicts

--- Documents/Escolta/server/index.js
+++ Documents/Escolta/server/index.js
@@ -9,7 +9,7 @@ const qrGeneratorRoutes = require('./routes/qr-generator');
 const { initDatabase } = require('./database/init');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
