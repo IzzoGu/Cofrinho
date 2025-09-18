59438b30f0fca869f63fe0eb8e4f07aec51d5e38
Gustavo Izzo Admin <_gustavo.izzo@internal.safewayconsultoria.com>
2025-09-18 19:13:18 -0300

    Fix API configuration to use localhost

--- Documents/Escolta/client/src/config/api.js
+++ Documents/Escolta/client/src/config/api.js
@@ -1,5 +1,3 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://192.168.0.35:5000' 
-  : 'http://localhost:5000';
+const API_BASE_URL = 'http://localhost:5000';
 
 export default API_BASE_URL;
